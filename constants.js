ae4eb02774b5e44e34c990db78ea1414a7c09401
angelDova <Angelfcb01@yahoo.com>
2022-12-09 12:26:20 -0800

    5th commit

---
+++ constants.js
@@ -0,0 +1,153 @@
+export const METADATA = {
+  title: "Portfolio | angelDova",
+  description:
+    "Angel Cordova is a Full Stack Developer from La Mirada, CA interested in crafting beautiful and functional applications.",
+  siteUrl: "https://shubhporwal.com/",
+  twitterHandle: "@angeldova",
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "home",
+  },
+  {
+    name: "Skills",
+    ref: "skills",
+  },
+  {
+    name: "Projects",
+    ref: "projects",
+  },
+  {
+    name: "Work",
+    ref: "work",
+  },
+  {
+    name: "Contact",
+    ref: "contact",
+  },
+];
+
+export const TYPED_STRINGS = [
+  "A pragmatic Full Stack Developer",
+  "I build things for the web",
+  "I create aesthetic and modern apps",
+];
+
+export const SOCIAL_LINKS = [
+  {
+    name: "mail",
+    url: "mailto: angelfcb01@yahoo.com",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/",
+  },
+  {
+    name: "github",
+    url: "https://github.com/angeldova",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/angeldova_/",
+  },
+  {
+    name: "twitter",
+    url: "https://twitter.com/",
+  },
+];
+
+export const SKILLS = {
+  languagesAndTools: [
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "sass",
+    "solidity",
+    // "cpp",
+    // "java",
+    // "python",
+    // "firebase",
+    "moralis",
+    // "stripe",
+    // "figma",
+  ],
+  librariesAndFrameworks: [
+    "react",
+    "redux",
+    "nextjs",
+    "tailwindcss",
+    "styledcomponents",
+    "antdesign",
+    "chakra-ui",
+  ],
+  databases: ["mysql", "mongodb"],
+  other: ["nodejs", "git", "sanity-io"],
+};
+
+export const PROJECTS = [
+  {
+    name: "Filmpire",
+    image: "/projects/filmpire.png",
+    blurImage: "/projects/blur/filmpire-blur.png",
+    description: "Netflix clone using ReactJS + CSS 🛏️",
+    gradient: ["#F14658", "#DC2537"],
+    url: "https://filmpire-angel.netlify.app/",
+    tech: ["react", "redux", "alan"],
+  },
+
+  {
+    name: "Ebay",
+    image: "/projects/ebay.png",
+    blurImage: "/projects/blur/ebay-blur.png",
+    description: "Ebay NFT clone using NextJS + TailwindCSS ✍🏻",
+    gradient: ["#FFA62E", "#EA4D2C"],
+    url: "https://ebay-angeldova.vercel.app/",
+    tech: ["typescript", "react", "nextjs", "tailwindcss", "thirdweb"],
+  },
+  {
+    name: "Gericht",
+    image: "/projects/gericht.png",
+    blurImage: "/projects/blur/gericht-blur.png",
+    description: "A Fine Dining Website Landing Page",
+    gradient: ["#000066", "#6699FF"],
+    url: "https://gericht-angeldova.netlify.app/",
+    tech: ["react", "chakra-ui", "alan"],
+  },
+  {
+    name: "GPT3",
+    image: "/projects/gpt3.png",
+    blurImage: "/projects/blur/gpt3-blur.png",
+    description: "An OpenAI Landing Page",
+    gradient: ["#142D46", "#2E4964"],
+    url: "https://gpt3-ai-angeldova.netlify.app/",
+    tech: ["react"],
+  },
+];
+
+export const WORK = [
+  {
+    company: "Renes Tire Service",
+    title: "Manager",
+    location: "Fontana, Ca",
+    range: "April 2011 - December 2019",
+    responsibilities: [
+      "Led the company in the organization and structure of sales.",
+      "Developed a customer first approach to every sale and transaction.",
+      "Led a team of 8 employees.",
+    ],
+    url: "https://google.com/",
+  },
+  {
+    company: "Uber",
+    title: "Driver",
+    location: "La Mirada, CA",
+    range: "December 2019 - December 2022",
+    responsibilities: ["Drive customers to and from their desired locations."],
+    url: "https://uber.com/",
+  },
+];
+
+export const GTAG = "G-5HCTL2TJ5W";
